2a8ec6969daeb9c5026e1b3ee43c3732b1f6ccee
ToF <cthibaut@octo.com>
2018-08-05 17:14:17 +0200

    refactoring: shorter names

--- src/lib.rs
+++ src/lib.rs
@@ -92,13 +92,13 @@ mod tests {
         #[test]
         #[ignore]
         fn should_find_a_ladder_between_two_words() {
-            let mut word_graph : WordGraph = WordGraph::default();
-            for s in ["CAT","CAT","BAT","BAG","COG","COT","DOG"].into_iter() {
-                word_graph.add_word(Word::from(s));
+            let mut graph : WordGraph = WordGraph::default();
+            for s in ["cat","cat","bat","bag","cog","cot","dog"].into_iter() {
+                graph.add_word(Word::from(s));
             }
-            let result = word_graph.ladder(Word::from("CAT"),
-                                           Word::from("DOG"));
-            let expected:Vec<Word> = vec!["CAT","COT","COG","DOG"].into_iter().map(Word::from).collect();
+            let result = graph.ladder(Word::from("cat"),
+                                           Word::from("dog"));
+            let expected:Vec<Word> = vec!["cat","cot","cog","dog"].into_iter().map(Word::from).collect();
             assert_eq!(result, expected)
         }
     }
@@ -108,16 +108,18 @@ mod tests {
 
         #[test]
         fn should_be_created_from_a_string_and_converted_into_a_string() {
-            assert_eq!(String::from("DOG"),Word::from("DOG").to_string());
-            assert_eq!(String::from("CAT"),Word::from("CAT").to_string())
+            let dog = Word::from("dog");
+            let cat = Word::from("cat");
+            assert_eq!(String::from("dog"),dog.to_string());
+            assert_eq!(String::from("cat"),cat.to_string())
         }
         #[test]
         fn should_be_comparable_for_equality() {
-            let w1 = Word::from("DOG");
-            let w2 = Word::from("DOG");
-            let w3 = Word::from("CAT");
-            assert_eq!(w1,w2);
-            assert_ne!(w1,w3)
+            let dog1= Word::from("dog");
+            let dog2 = Word::from("dog");
+            let cat = Word::from("cat");
+            assert_eq!(dog1,dog2);
+            assert_ne!(dog1,cat)
         }
 
         mod is_adjacent {
@@ -125,27 +127,39 @@ mod tests {
 
             #[test]
             fn should_be_false_if_words_are_identical() {
-                let w = Word::from("DOG");
-                assert_eq!(false, w.is_adjacent(Word::from("DOG")))
+                let dog = Word::from("dog");
+                assert_eq!(false, dog.is_adjacent(dog))
             }
             #[test]
             fn should_be_true_if_words_are_different_by_their_last_letter() {
-                assert_eq!(true, Word::from("DOG").is_adjacent(Word::from("DOT")))
+                let dog = Word::from("dog");
+                let dot = Word::from("dot");
+                assert_eq!(true, dog.is_adjacent(dot));
             }
             #[test]
             fn should_be_true_if_words_are_different_by_only_a_letter() {
-                assert_eq!(true, Word::from("DOG").is_adjacent(Word::from("DIG")));
-                assert_eq!(true, Word::from("DOG").is_adjacent(Word::from("FOG")));
+                let dog = Word::from("dog");
+                let dig = Word::from("dig");
+                let fog = Word::from("fog");
+                assert_eq!(true, dog.is_adjacent(dig));
+                assert_eq!(true, dog.is_adjacent(fog));
             }
             #[test]
             fn should_be_false_if_words_are_different_by_more_than_one_letter() {
-                assert_eq!(false, Word::from("DOG").is_adjacent(Word::from("DIB")));
-                assert_eq!(false, Word::from("DOG").is_adjacent(Word::from("GOT")));
+                let dog = Word::from("dog");
+                let dib = Word::from("dib");
+                let got = Word::from("got");
+                assert_eq!(false, dog.is_adjacent(dib));
+                assert_eq!(false, dog.is_adjacent(got));
             }
             #[test]
             fn should_be_false_if_words_have_different_size() {
-                assert_eq!(false, Word::from("DOGE").is_adjacent(Word::from("DOG")));
-                assert_eq!(false, Word::from("BAT").is_adjacent(Word::from("BATH")));
+                let dog = Word::from("dog");
+                let doge = Word::from("doge");
+                let bat = Word::from("bat");
+                let bath = Word::from("bath");
+                assert_eq!(false, doge.is_adjacent(dog));
+                assert_eq!(false, bat.is_adjacent(bath));
             }
         }
     }
@@ -156,41 +170,45 @@ mod tests {
 
         #[test]
         fn should_not_contain_a_word_when_empty() {
-            let word_graph = WordGraph::default();
-            assert_eq!(Unknown, word_graph.get(Word::from("DOG")))
+            let graph = WordGraph::default();
+            let dog = Word::from("dog");
+            assert_eq!(Unknown, graph.get(dog))
         }
         #[test]
         fn should_contain_an_unmarked_word_that_was_added() {
-            let mut word_graph: WordGraph = WordGraph::default();
-            word_graph.add_word(Word::from("DOG"));
-            assert_eq!(Unmarked, word_graph.get(Word::from("DOG")));
-            assert_eq!(Unknown, word_graph.get(Word::from("CAT")))
+            let dog = Word::from("dog");
+            let cat = Word::from("cat");
+            let mut graph: WordGraph = WordGraph::default();
+            graph.add_word(dog);
+            assert_eq!(Unmarked, graph.get(dog));
+            assert_eq!(Unknown, graph.get(cat))
         }
         #[test]
         fn should_mark_a_word_as_linked_to_another() {
-            let mut word_graph: WordGraph = WordGraph::default();
-            word_graph.add_word(Word::from("DOG"));
-            word_graph.add_word(Word::from("FOG"));
-            word_graph.link(Word::from("FOG"),Word::from("DOG"));
-            assert_eq!(Linked (Word::from("DOG")), word_graph.get(Word::from("FOG")))
+            let dog = Word::from("dog");
+            let fog = Word::from("fog");
+            let mut graph: WordGraph = WordGraph::default();
+            graph.add_word(dog);
+            graph.add_word(fog);
+            graph.link(fog,dog);
+            assert_eq!(Linked(dog), graph.get(fog))
         }
         #[test]
         fn should_find_a_one_step_path_to_an_unmarked_word() {
-            let mut word_graph: WordGraph = WordGraph::default();
-            word_graph.add_word(Word::from("DOG"));
-            let expected = vec![Word::from("DOG")];
-            let result = word_graph.path(Word::from("DOG"));
-            assert_eq!(result, expected)
+            let dog = Word::from("dog");
+            let mut graph: WordGraph = WordGraph::default();
+            graph.add_word(dog);
+            assert_eq!(vec![dog], graph.path(dog))
         }
         #[test]
         fn should_find_a_two_step_path_to_a_unmarked_word() {
-            let mut word_graph: WordGraph = WordGraph::default();
-            word_graph.add_word(Word::from("DOG"));
-            word_graph.add_word(Word::from("FOG"));
-            word_graph.link(Word::from("FOG"),Word::from("DOG"));
-            let expected = vec![Word::from("FOG"),Word::from("DOG")];
-            let result = word_graph.path(Word::from("FOG"));
-            assert_eq!(result, expected)
+            let dog = Word::from("dog");
+            let fog = Word::from("fog");
+            let mut graph: WordGraph = WordGraph::default();
+            graph.add_word(dog);
+            graph.add_word(fog);
+            graph.link(fog,dog);
+            assert_eq!(vec![fog,dog],graph.path(fog))
         }
     }
 
