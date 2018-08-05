2498c7b4f4a1e1cc5257158a36fc1a1168ac149c
ToF <cthibaut@octo.com>
2018-08-05 09:15:23 +0200

    2nd Loop test compiles

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,27 @@
 
+#[derive(Debug,PartialEq)]
+struct Word {
+    inner: u64
+}
+
+impl Word {
+    pub fn from(_s: &str) -> Self {
+        Word { inner: 0 }
+
+    }
+}
+
 #[derive(Default)]
 struct WordGraph {}
 
+impl WordGraph {
+    pub fn add_word(&self, _word : Word) {
+    }
+
+    pub fn ladder(&self, _origin: Word, _target: Word) -> Vec<Word> {
+        vec![]
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -18,7 +38,7 @@ mod tests {
             }
             let result = word_graph.ladder(Word::from("CAT"),
                                            Word::from("DOG"));
-            let expected = vec!["CAT","COT","COG","DOG"].into_iter().map(Word::from).collect();
+            let expected:Vec<Word> = vec!["CAT","COT","COG","DOG"].into_iter().map(Word::from).collect();
             assert_eq!(result, expected)
         }
     }
