9237ecc4715c81f1bbdf5da835bca827825e6e3b
ToF- <cthibaut@octo.com>
2018-08-06 19:02:14 +0200

    filter words on origin length added a word file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate structopt;
+
+mod word_graph;
+
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
+use structopt::StructOpt;
+use word_graph::WordGraph;
+use word_graph::Word;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "word_ladder", about = "Finding ladders between words.")]
+struct Opt {
+    /// Path to the word dictionnary
+    #[structopt(parse(from_os_str))]
+    words_input: PathBuf,
+    /// Origin of the ladder
+    origin: String,
+    /// Target of the ladder
+    target: String,
 }
+
+fn main() {
+    let opt = Opt::from_args();
+    let origin = &opt.origin;
+    let target = &opt.target;
+    let mut file = File::open(opt.words_input).expect("file should exists");
+    let mut content = String::with_capacity(128_000);
+    file.read_to_string(&mut content).expect("file should be readable");
+    let mut graph : WordGraph = content.lines().filter(|s| s.len() == origin.len()).map(Word::from).collect();
+    let origin = Word::from(&opt.origin);
+    let target = Word::from(&opt.target);
+    let output : Vec<String>= graph.ladder(origin, target).into_iter().map(|w| w.to_string()).collect();
+    println!("{:?}", output);
+}
+
